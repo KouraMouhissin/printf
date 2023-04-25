8dbd749597e013d824178ce30c3ab425d3116fb4
KouraMouhissin <mouhissin@gmail.com>
2023-04-25 08:46:38 -0700

    _printf.c file added

---
+++ test/0-main.c
@@ -0,0 +1,21 @@
+#include "myheader.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+	int nbr;
+
+	nbr = 19912507;
+
+	c = _printf("%i, %d\n", nbr, nbr);
+	printf("%d\n", c);
+	c = _printf("% ");
+	printf("%d\n", c);
+	return (0);
+}
--- test/main.c
+++ test/main.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
+#include "myheader.h"
 
 /**
  * main - Entry point
