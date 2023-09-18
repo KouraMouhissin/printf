a34a8a27df50d2752074192acbc1cadf9c16d6eb
KouraMouhissin <mouhissin@gmail.com>
2023-09-18 05:15:17 -0700

    First printf project commit

--- main.h
+++ main.h
@@ -1,25 +1,43 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <math.h>
+
 /**
- * struct format - for matching convertion specificies
- * @id: char pointer
- * @f: a pointer to function for conv
- */
-
-typedef struct format
+ * struct specifier_manager - struct to manage specifiers
+ * @specifier: input specifier
+ * @f: function pointer to print function
+*/
+typedef struct specifier_manager
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	char *specifier;
+	int (*f)(va_list);
+} specifier_manager_t;
 
-int _putchar(char c);
+/* print_functions.c */
+int print_char(va_list);
+int print_string(va_list);
+int print_string_interval(char *start, char *end);
+int print_int(va_list);
+int print_percentage(va_list);
+
+/* put_functions.c */
+int _puts(char *s);
+int _putchar(int n);
+
+/* specifier_functions.c */
+int (*get_specifier(char *s))(va_list);
+int get_print_function(char *s, va_list);
+
+/* _tostring.c */
+char *int_to_string(int num);
+
+/* _printf.c */
 int _printf(const char *format, ...);
 
-#endif
+#endif /* _MAIN_H_ */
